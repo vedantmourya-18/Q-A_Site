fa0eceaf89ab18e470b5c69fce66329f2999733e
vedantmourya-18 <vedantmourya18@gmail.com>
2021-04-24 21:11:51 +0530

    Column added

--- frontend/lib/loading_screen.dart
+++ frontend/lib/loading_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(),
+    );
   }
 }
